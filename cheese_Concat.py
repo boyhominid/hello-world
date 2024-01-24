10fb6a15f2a25615fdec95f5bb414bbeea6d36bd
S. D <143271691+boyhominid@users.noreply.github.com>
2024-01-24 04:59:47 -0500

    Create cheese_Concat.py
    Added cheese_Concat

---
+++ cheese_Concat.py
@@ -0,0 +1,7 @@
+cheese_span = 8
+cheese_descrip = "This cheese is huge!"
+cheesier_descrip = "This cheese is freaky-deaky huuuge!"
+
+if cheese_span < 7: print(cheese_descrip)
+
+if cheese_span >= 7: print(cheesier_descrip)
